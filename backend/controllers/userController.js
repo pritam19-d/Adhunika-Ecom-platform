37f6bb5ff3566d95055a44914a0d3d9f30116fc2
pritam19-d <125026007+pritam19-d@users.noreply.github.com>
2024-05-27 03:15:38 +0530

    implemented  backend authentication complete functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -96,15 +96,23 @@ const updateUserProfile = asyncHandler (async (req, res)=>{
   const user = await User.findById(req.user._id)
 
   if(user){
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    if(req.body.password){
+      user.password = req.body.password
+    }
+    
+    const updateUser = await user.save()
     res.status(200).json({
-      _id: user._id,
+      _id: updateUser._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin
     })
-  }
-  // res.send("Update user Profile.")
-})
+  } else{
+      res.status(404)
+      throw new Error("User not found")
+  }})
 
 //@desc   GET Users
 //@route  GET/api/users/
