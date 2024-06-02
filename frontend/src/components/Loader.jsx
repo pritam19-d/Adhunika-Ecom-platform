b89e0aae1f5f5e9fa5d1376d6428626f733ca95d
pritam19-d <125026007+pritam19-d@users.noreply.github.com>
2024-06-02 11:32:29 +0530

    Bug Fixed: 'Loader and Message isnt working properly'

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { Spinner } from "react-bootstrap";
 
-const Loader = ()=>{
-  <Spinner
-    color="blue"
-    animation="border"
-    role="status"
-    style={{
-      width: "100px",
-      height: "100px",
-      margin: "auto",
-      display: "block"
-    }}
-  >Please Wait...</Spinner>
+const Loader = () => {
+  return (
+    <Spinner
+      color="blue"
+      animation="border"
+      role="status"
+      style={{
+        width: "100px",
+        height: "100px",
+        margin: "auto",
+        display: "block"
+      }}
+    ></Spinner>
+  )
 }
 
 export default Loader
--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Alert } from "react-bootstrap"
 
-const Message = ({ varient, children }) => {
+const Message = ({ variant, children }) => {
   return (
-    <Alert variant={varient}>
+    <Alert variant={variant}>
       {children}
     </Alert>
   )
