3cb59d44fa7494855eb39447c972343066d8162f
pritam19-d <125026007+pritam19-d@users.noreply.github.com>
2024-07-19 09:32:13 +0530

    Development: Products Page Created for Admins only.

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { Link, useParams } from "react-router-dom"
-import { Row, Col, ListGroup, Image, Form, Button, Card } from "react-bootstrap"
+import { Row, Col, ListGroup, Image, Button, Card } from "react-bootstrap"
 import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js"
 import Message from "../components/Message"
 import Loader from "../components/Loader"
--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,9 +1,67 @@
 import React from 'react'
+import { LinkContainer } from "react-router-bootstrap"
+import { Table, Button, Row, Col } from "react-bootstrap"
+import { FaTimes, FaEdit, FaTrash } from "react-icons/fa"
+import Message from "../../components/Message"
+import Loader from "../../components/Loader"
+import { useGetProductsQuery } from "../../slicers/productApiSlice"
 
 const ProductListScreen = () => {
+  const { data: products, isLoading, error } = useGetProductsQuery()
+
+  const deleteHandler = (id)=>{
+    console.log("delete", id)
+  }
+
   return (
     <div>
-      Product Screen
+      <Row className="align-items-center">
+        <Col>
+          <h1>All Products</h1>
+        </Col>
+        <Col className="text-end">
+          <Button className="btn-sm btn-dark m-3">
+            <FaEdit /> Create Product
+          </Button>
+        </Col>
+      </Row>
+      {isLoading ? <Loader /> : error ? <Message variant="danger">{error}</Message> : (
+        <>
+          <Table striped hover responsive className="table-sm">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>PRICE</th>
+                <th>CATEGORY</th>
+                <th>STOCK COUNT</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product)=>(
+                <tr key={product._id}>
+                  <td>{product._id}</td>
+                  <td>{product.name}</td>
+                  <td>₹{product.price}</td>
+                  <td>{product.category}</td>
+                  <td>{product.countInStock}</td>
+                  <td>
+                    <LinkContainer to={`admin/products/${product._id}/edit`}>
+                      <Button variant="light" className="btn-sm mx-2">
+                        <FaEdit />
+                      </Button>
+                    </LinkContainer>
+                      <Button variant="light" className="btn-sm mx-2" style={{"color":"black"}} onClick={()=>deleteHandler(product._id)}>
+                        <FaTrash />
+                      </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </>
+      )}
     </div>
   )
 }
