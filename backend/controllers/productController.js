932cfa195821f4b981be756962b42e61a2fb1b76
pritam19-d <125026007+pritam19-d@users.noreply.github.com>
2024-07-27 16:05:30 +0530

    Updated carousel

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,7 +5,7 @@ import Product from "../models/productModel.js";
 //@route  GET /api/products
 //@access Public
 const getProducts = asyncHandler (async (req, res)=>{
-  const pageSize = 2;
+  const pageSize = 4;
   const page = Number(req.query.pageNumber);
   const keyword = req.query.keyword ? {name: {$regex: req.query.keyword, $options: "i" }} : {}
 
@@ -123,7 +123,7 @@ const createProductReview = asyncHandler (async (req, res)=>{
 //@route  GET /api/products/top
 //@access Public
 const getTopProducts = asyncHandler(async (req, res)=>{
-  const products = await Product.find({}).sort({rating:-1}).limit(3);
+  const products = await Product.find({}).sort({rating:-1}).limit(4);
   res.status(200).json(products)
 })
 
